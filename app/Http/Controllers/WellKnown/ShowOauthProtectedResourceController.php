5eb9cc37549a87edac6175c48f67cb8a52d10941
Tom Witkowski <dev.gummibeer@gmail.com>
2026-05-20 12:20:50 +0200

    Update OAuth protected resource URL to `/mcp`.

--- app/Http/Controllers/WellKnown/ShowOauthProtectedResourceController.php
+++ app/Http/Controllers/WellKnown/ShowOauthProtectedResourceController.php
@@ -10,7 +10,7 @@ class ShowOauthProtectedResourceController
     public function __invoke(): JsonResponse
     {
         return response()->json([
-            'resource' => url('/'),
+            'resource' => url('/mcp'),
             'authorization_servers' => [URL::toApp('/')],
             'scopes_supported' => config('services.archivist.oauth_scopes_supported'),
             'bearer_methods_supported' => ['header'],
